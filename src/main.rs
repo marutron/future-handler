6c2c060402f776855a1857dddd737daba0a6567a
marat <ahmatov-marat8@mail.ru>
2025-03-05 10:16:47 +0400

    finished

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,47 @@ struct Executor {
     ready_queue: Receiver<Arc<Task>>,
 }
 
+impl Executor {
+    fn run(&self) {
+        println!("run goes");
+        while let Ok(task) = self.ready_queue.recv() {
+            let mut future_slot = task.future.lock().unwrap();
+            if let Some(mut future) = future_slot.take() {
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&waker);
+                if future.as_mut().poll(context).is_pending() {
+                    *future_slot = Some(future);
+                }
+            }
+        }
+    }
+}
+
 struct Spawner {
     task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output=()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.try_send(task).expect("too many tasks queued");
+    }
 }
 
 struct Task {
     future: Mutex<Option<BoxFuture<'static, ()>>>,
     task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let cloned = arc_self.clone();
+        arc_self.task_sender.try_send(cloned).expect("too many tasks queued");
+    }
 }
 
 fn new_executor_and_spawner() -> (Executor, Spawner) {
@@ -31,6 +65,21 @@ fn new_executor_and_spawner() -> (Executor, Spawner) {
     (Executor { ready_queue }, Spawner { task_sender })
 }
 
-fn main() {}
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+    spawner.spawn(async {
+        println!("first go!");
+        TimerFuture::new(Duration::new(5, 0)).await;
+        println!("first done!");
+    });
+
+    spawner.spawn(async {
+        println!("second go!");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("second done!");
+    });
+    drop(spawner);
+    executor.run();
+}
 
 
